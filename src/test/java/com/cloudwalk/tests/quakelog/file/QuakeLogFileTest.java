455a8e1ba8e669683326545c57de1c8a62a46753
Eric Rosetti Lessa <eric.lessa@arcelormittal.com.br>
2022-07-06 13:47:06 -0300

    ajuste no test QuakeLogFileTest

--- src/test/java/com/cloudwalk/tests/quakelog/file/QuakeLogFileTest.java
+++ src/test/java/com/cloudwalk/tests/quakelog/file/QuakeLogFileTest.java
@@ -1,5 +1,7 @@
 package com.cloudwalk.tests.quakelog.file;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.FileNotFoundException;
 import java.util.List;
 
@@ -7,20 +9,23 @@ import org.junit.Test;
 
 import com.cloudwalk.tests.quakelog.game.Game;
 import com.cloudwalk.tests.quakelog.game.GameRepository;
-import com.cloudwalk.tests.quakelog.report.GameReport;
 
 public class QuakeLogFileTest{
 
 	@Test
 	public void testGameInFile() throws FileNotFoundException {
-		
+
+		//arrange
 		GameRepository gameRepository = new QuakeLogFile("qgames.log");
 		
+		//act
 		List<Game> games = gameRepository.findAll();
+		Game game = games.iterator().next();
 
-		GameReport.print(games);
-
-		
+		//verify
+		assertEquals(1, games.size());
+		assertEquals(8, game.players().size());
+		assertEquals(20, game.totalKills());
 	}
 
 }
